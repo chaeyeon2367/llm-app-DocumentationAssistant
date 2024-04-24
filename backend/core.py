aca238baefb4dd26bd354564b0121f3438fdc2f0
Shim chaeyeon <chaeyeon.shim@etu.univ-cotedazur.fr>
2024-04-24 17:38:05 +0200

    implemented RetrievalQA chain

---
+++ backend/core.py
@@ -0,0 +1,26 @@
+import os
+from typing import Any
+
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain.chains import RetrievalQA
+from langchain_pinecone import Pinecone
+from langchain_pinecone.vectorstores import PineconeVectorStore
+
+
+def run_llm(query: str) -> Any:
+    embeddings = OpenAIEmbeddings()
+    docsearch = Pinecone.from_existing_index(
+        index_name="langchain-doc-index", embedding=embeddings
+    )
+    chat = ChatOpenAI(verbose=True, temperature=0)
+    qa = RetrievalQA.from_chain_type(
+        llm=chat,
+        chain_type="stuff",
+        retriever=docsearch.as_retriever(),
+        return_source_documents=True,
+    )
+    return qa({"query": query})
+
+
+if __name__ == "__main__":
+    print(run_llm(query="What is LangChain?"))
